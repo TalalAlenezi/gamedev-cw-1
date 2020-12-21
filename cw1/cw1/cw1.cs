22877c42a5add63167390c7f96ead3d45fb81daa
TalalAlenezi <76274645+TalalAlenezi@users.noreply.github.com>
2020-12-21 17:42:25 +0300

    cw-2

--- cw1/cw1/cw1.cs
+++ cw1/cw1/cw1.cs
@@ -12,8 +12,18 @@ namespace work1
             string superPower1 = "Steel Armor";
             string superPower2 = "Night Vision";
 
-            Console.WriteLine("My Name is {0}, my super powers are {1} and {2}. I'm {3} years old and my speed is {4}", characterName, superPower1, superPower2, age, speed);
+            //Second hero
+            string heroName = "Link";
+            double heroHeight = 175.0;
+            int heroAge = 17;
+            string heroSuperPower1 = "Arrows";
+            string heroSuperPower2 = "Master Sword";
 
+            int ageDifference = heroAge - age;
+
+
+            //Console.WriteLine("My Name is {0}, my super powers are {1} and {2}. I'm {3} years old and my speed is {4}", characterName, superPower1, superPower2, age, speed);
+            Console.WriteLine("The Age difference is {0}", ageDifference);
 
         }
     }
